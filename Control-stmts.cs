861c6c544229f771454846109d1c90dca20bb998
Jahnavilingutla <lingutlajahnavi19@gmail.com>
2022-09-28 22:23:53 +0530

    Add project files.

---
+++ Control-stmts.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_SHARP
+{
+    internal class Control_stmts
+    {
+        public static void Main()
+        {
+            //if condition
+            int num = 10;
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("It is even number");
+            }
+            //if else condition
+            int num1 = 11;
+            if (num1 % 2 == 0)
+            {
+                Console.WriteLine("It is even number");
+            }
+            else
+            {
+                Console.WriteLine("It is odd number");
+            }
+            //FOR LOOP
+            int n;
+            int i;
+            int sum = 0;
+            Console.WriteLine("Enter the value of n");
+            n = Convert.ToInt32(Console.ReadLine()); 
+           
+            for (i = 1; i <= n; i++) 
+            {
+                sum += i; 
+               
+
+            }
+            Console.WriteLine("Sum is:{0}", sum); 
+
+            //WHILE LOOP
+            Console.WriteLine("WHILE");
+            int j = 1;
+            while (j < 5) 
+            {
+                Console.WriteLine("The value is:{0}", j); 
+                j++;
+            }
+
+            //DO-WHILE LOOP
+            Console.WriteLine("DO-WHILE");
+            int k = 1;
+            do
+            {
+               
+                Console.WriteLine("The value is:{0}", k);
+                k++;
+            } while (k < 1);
+
+            //foreach
+            Console.WriteLine("FOREACH");
+            string course = "CSHARP";
+            foreach (char c in course)
+            {
+                Console.WriteLine(c);
+                //break and continue
+                //break;
+                //continue;
+                if (c == 'A')
+                {
+                    //break;
+                    continue;
+                }
+                Console.WriteLine("Hello");
+            }
+
+        }
+
+
+    }
+}
---
+++ methods.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//WAP to accept all the employee details using static method and 
+//display the details in an instance method:
+
+namespace C_SHARP
+{
+    /// <summary>
+    /// static & instance methods
+    /// </summary>
+    internal class methods
+    {
+       
+
+        public static void Main()
+        {
+
+            Staticmethod();
+            
+        }
+        //STATIC METHOD
+        public static void Staticmethod()
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("Enter Employee ID");
+                int EMPID = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter Employee name");
+                string Empname = Console.ReadLine();
+                Console.WriteLine("Enter Employee designation");
+                string Edesig = Console.ReadLine();
+                methods m = new methods();
+                m.Instancemethod(EMPID, Empname, Edesig);
+            }
+
+           
+
+        }
+        public void Instancemethod(int EMPID, string Empname, string Edesig)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("Emp id is: " + EMPID);
+                Console.WriteLine("Emp name is: " + Empname);
+                Console.WriteLine("Emp desig is: " + Edesig);
+
+            }
+               
+
+        }
+    }
+}
